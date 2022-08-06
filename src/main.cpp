580750eb4bc171746637667f4baeda03f82e5616
Carl <carlmesias20@gmail.com>
2022-08-06 11:40:16 -0700

    Removed debug cout lines.

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,30 @@ public:
 
 vector<Users> users;
 
-// Register: Check if username and password matches an existing user, if so prompt again
-bool CheckIfRegisterInputMatchesDatabase(stringstream userNamePassword) {
+// PROMPT: user to select an option from the menu
+int promptMenuSelection() {
 
+	int option = 4;
+
+	// Dispaly options for user to choose from
+	cout << "-----------------------------------------" << endl;
+	cout << "                   MENU                  " << endl;
+	cout << "-----------------------------------------" << endl;
+	cout << "0. Login "									<< endl;
+	cout << "1. Register" 								<< endl;
+	cout << "2. Forgot password" 						<< endl;
+	cout << "3. Quit" 									<< endl;
 	cout << endl;
-	cout << "-----------------------------------------" << endl;
-	cout << "            REGISTER: ENTERED            " << endl;
-	cout << "-----------------------------------------" << endl;
+
+	// Get user input
+	cin >> option;
+
+	// Return selected option
+	return option;
+}
+
+// REGISTER: Check if username and password matches an existing user
+bool CheckIfRegisterInputMatchesDatabase(stringstream userNamePassword) {
 
 	// Create temporary variables to store registered info
 	vector<Users> buffer;
@@ -42,28 +59,18 @@ bool CheckIfRegisterInputMatchesDatabase(stringstream userNamePassword) {
 	    }
 	}
 
-	// Print out database of users from vector
-	//cout << endl;
-	//cout << "-----------------------------------------" << endl;
-	//cout << "   REGISTER: SHOW DATABASE FROM VECTOR   " << endl;
-	//cout << "-----------------------------------------" << endl;
-
 	// Check if username and password matches an existing user
 	for (unsigned int i = 0; i < buffer.size(); i++) {
-		//cout << "i: " << i << ", Username: " << buffer[i].username << ", Password: " << buffer[i].password << endl;
-
-		// Compare input username and password with database of users if it exists
 
 		// Username matches existing username in database
 		if (usernameTemp == buffer[i].username) {
-
-			// Return that user exists already in database
 			return true;
 		}
 	}
 
-	// If we were not able to return from the above code, that means username & password inputted does not match our records, create new user
+	// If we were not able to return from the above code, that means username & password inputted does not exist in our records, create new user
 	{
+		cout << endl;
 		cout << "-----------------------------------------" << endl;
 		cout << "       REGISTER: CREATING NEW USER       " << endl;
 		cout << "-----------------------------------------" << endl;
@@ -98,25 +105,13 @@ bool CheckIfRegisterInputMatchesDatabase(stringstream userNamePassword) {
 				}
 			}
 			fileOpen.close();
-
-			// Return that user did not exist, so we created a new user
 			return false;
 		}
 	}
-	cout << "-----------------------------------------" << endl;
-	cout << "            REGISTER: EXITING            " << endl;
-	cout << "-----------------------------------------" << endl;
 }
 
-// Login: Check if username and password matches an existing user
+// LOGIN: Check if username and password matches an existing user
 bool CheckIfLoginMatchesDatabase(stringstream userNamePassword) {
-	cout << endl;
-	cout << endl;
-	cout << "-----------------------------------------" << endl;
-	cout << "              LOGIN: ENTERED             " << endl;
-	cout << "-----------------------------------------" << endl;
-
-	bool userExists = false;
 
 	// Create temporary variables to store ogin info
 	string usernameTemp;
@@ -136,64 +131,27 @@ bool CheckIfLoginMatchesDatabase(stringstream userNamePassword) {
 	    }
 	}
 
-	// Print out database of users from vector
-	//cout << endl;
-	//cout << "-----------------------------------------" << endl;
-	//cout << "    LOGIN: SHOW DATABASE FROM VECTOR     " << endl;
-	//cout << "-----------------------------------------" << endl;
-
 	// Check if username and password matches an existing user
 	for (unsigned int i = 0; i < users.size(); i++) {
-		//cout << "i: " << i << ", Username: " << users[i].username << ", Password: " << users[i].password << endl;
-
-		// Compare input username and password with database of users if it exists
 
 		// Username matches existing username in database
 		if (usernameTemp == users[i].username) {
 
 			// Password also matches existing username in database, stop looping and return true statement
 			if (passwordTemp == users[i].password) {
-				userExists = true;
-
-				// Return that user exists in database
-				return userExists;
+				return true;
 			}
 		}
 	}
 
-	// If we were not able to return from the above code, that means username & password inputted does not match our records, return false
-	return userExists;
-	cout << "-----------------------------------------" << endl;
-	cout << "              LOGIN: EXITING             " << endl;
-	cout << "-----------------------------------------" << endl;
+	// Return that user DOES NOT exists in database
+	return false;
 }
 
-// Prompt user to select an option from the menu
-int promptMenuSelection() {
-
-	int option = 4;
-
-	// Dispaly options for user to choose from
-	cout << "-----------------------------------------" << endl;
-	cout << "                   MENU                  " << endl;
-	cout << "-----------------------------------------" << endl;
-	cout << "0. Login "									<< endl;
-	cout << "1. Register" 								<< endl;
-	cout << "2. Forgot password" 						<< endl;
-	cout << "3. Quit" 									<< endl;
-	cout << endl;
-
-	// Get user input
-	cin >> option;
-
-	// Return selected option
-	return option;
-}
-
-// Login: get user input for username and password
+// LOGIN: Get user input for username and password
 stringstream getLoginUsernameAndPassword() {
 
-	// Return user ogin info
+	// Return user login info
 	stringstream usernamePasswordLogin;
 
 	cout << "-----------------------------------------" << endl;
@@ -210,14 +168,10 @@ stringstream getLoginUsernameAndPassword() {
 	// Store usernme and password input into stringstream for return
 	usernamePasswordLogin << username + " " + password;
 
-	// Show user their username and password
-	cout << endl;
-	cout << "Login info: " << usernamePasswordLogin.str().c_str() << endl;
-
 	return usernamePasswordLogin;
 }
 
-// Register: get user input for username and password
+// REGISTER: Get user input for username and password
 stringstream getRegisterUsernameAndPassword() {
 
 	// Return user registered info
@@ -238,29 +192,34 @@ stringstream getRegisterUsernameAndPassword() {
 	// Return username and password
 	usernamePasswordRegister << username + " " + password;
 
-	// Show user their username and password
-	cout << endl;
-	cout << "Registered info: " << usernamePasswordRegister.str().c_str() << endl;
-
 	return usernamePasswordRegister;
 }
 
 void LogInSuccessOrFailure(bool success) {
 	cout << endl;
+	cout << "-----------------------------------------" << endl;
+	cout << "              LOGIN result:              " << endl;
+	cout << "-----------------------------------------" << endl;
 	if (success) {
 		cout << "Successfully logged in!" << endl;
 	} else {
 		cout << "Username and Password do not match our records. :(" << endl;
 	}
+	cout << endl;
 }
 
 void RegisteredSuccessOrFailure(bool existingUser) {
+	cout << endl;
+	cout << "-----------------------------------------" << endl;
+	cout << "            REGISTER result:             " << endl;
+	cout << "-----------------------------------------" << endl;
 	cout << endl;
 	if (existingUser) {
 		cout << "Username already exists! Please log in." << endl;
 	} else {
 		cout << "Registered Successfully!" << endl;
 	}
+	cout << endl;
 
 }
 
